c505b9908afca6f6189049f8320b74764f94fe55
chandransellappan <sel.chandru@gmail.com>
2021-07-13 08:30:19 -0500

    Update the project with SpringBoot base configurations

---
+++ src/main/java/com/chandran/july/DailyLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.chandran.july;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyLearningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandran/july/DailyLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandran.july;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
