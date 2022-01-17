503486636f52afb0ea1e6cacbf0d0948823fd8b9
chandransellappan <sel.chandru@gmail.com>
2022-01-17 10:53:04 -0600

    Adding a new Project for Home Automation

---
+++ HomeAutomation/src/main/java/com/home/automation/HomeAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.home.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAutomationApplication.class, args);
+	}
+
+}
